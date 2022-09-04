9d918d1a4d09673ece59fbcec0436cd18d990d31
Simona Ristovska <simona.ristovska@blackboard.com>
2022-09-04 12:24:40 +0200

    passing and retrieving data with actions on dispatch

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -8,6 +8,24 @@ const reducer = (state = initialState, action) => {
             counter: state.counter + 1
         }
     }
+
+    if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+
+    if (action.type === 'ADD') {
+        return {
+            counter: state.counter + action.value
+        }
+    }
+
+    if (action.type === 'SUBTRACT') {
+        return {
+            counter: state.counter - action.value
+        }
+    }
     return state;
 };
 
